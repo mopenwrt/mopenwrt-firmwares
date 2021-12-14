f5ceb244e3f8e675e071a040ea85b11c55e9e918
Riceball LEE <snowyu.lee@gmail.com>
2021-12-14 20:23:34 +0800

    fix: forget to add-packges

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -6,3 +6,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 /bin/bash "${SCRIPT_DIR}/customfeeds.sh"
 ./scripts/feeds install -a
+
+/bin/bash "${SCRIPT_DIR}/add-packages.sh"
